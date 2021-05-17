be792596def6e357e9901e3869ab720c63effe4c
Nikhil <nikhilanthonyhenry@gmail.com>
2021-05-17 09:01:14 +0530

    init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+}
+</style>
