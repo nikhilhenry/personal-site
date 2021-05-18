6d5ceba85a220a46ac28a9e432c43fbfe67fe518
Nikhil <nikhilanthonyhenry@gmail.com>
2021-05-18 13:25:47 +0530

    install font awesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,9 @@ export default {
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script:[
+      {src:"https://kit.fontawesome.com/a40ef71e31.js"}
+    ]
   },
   /*
    ** Global CSS
