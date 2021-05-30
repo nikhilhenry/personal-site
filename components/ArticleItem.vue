9338a5b126eb5abc825026dd4374a7454facf72c
Nikhil <nikhilanthonyhenry@gmail.com>
2021-05-30 10:40:18 +0530

    remove wrapper element

--- components/ArticleItem.vue
+++ components/ArticleItem.vue
@@ -1,36 +1,33 @@
 <template>
   <article class="article">
-    <nuxt-link to="/articles/test_article" class="wrapper">
-      <img
-        src="https://images.unsplash.com/photo-1621675110684-84f7d1914cdc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2148&q=80"
-        class="cover-img"
-      />
-      <div class="content">
-        <h2 class="title">Golden Gate Bridge</h2>
-        <section class="stats">
-          <span class="date">May 4, 2021</span>
-          <span class="duration">
-            <i class="far fa-clock"></i>
-            <span>5 min read</span>
-          </span>
-          <nuxt-link
-            v-if="!inCollection"
-            to="/collections/exploration"
-            class="series"
-            >#exploration</nuxt-link
-          >
-        </section>
-        <p class="excerpt">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt...
-        </p>
-      </div>
-    </nuxt-link>
+    <img
+      src="https://images.unsplash.com/photo-1621675110684-84f7d1914cdc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2148&q=80"
+      class="cover-img"
+    />
+    <div class="content">
+      <h2 class="title">Golden Gate Bridge</h2>
+      <section class="stats">
+        <span class="date">May 4, 2021</span>
+        <span class="duration">
+          <i class="far fa-clock"></i>
+          <span>5 min read</span>
+        </span>
+        <nuxt-link
+          v-if="!inCollection"
+          to="/collections/exploration"
+          class="series"
+          >#exploration</nuxt-link
+        >
+      </section>
+      <p class="excerpt">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt...
+      </p>
+    </div>
   </article>
 </template>
 
