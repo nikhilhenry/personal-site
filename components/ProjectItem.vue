d82dd848ec5f60c419e4d63ec99973e051683f27
Nikhil <nikhilanthonyhenry@gmail.com>
2021-05-30 12:57:07 +0530

    add buttons

--- components/ProjectItem.vue
+++ components/ProjectItem.vue
@@ -16,6 +16,22 @@
           <li class="stack go">go</li>
         </ul>
       </div>
+      <div class="description">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <div class="btn-group">
+        <a href="#" class="btn source"
+          ><i class="fas fa-code-branch"></i> view source</a
+        >
+        <a href="#" class="btn build"
+          ><i class="fas fa-terminal"></i> view build</a
+        >
+      </div>
     </div>
   </article>
 </template>
@@ -45,7 +61,7 @@
   }
 
   .technologies {
-    margin-top: 1rem;
+    margin: 1rem 0 1.5rem;
     .stacks {
       list-style-type: none;
       margin: 0;
@@ -61,6 +77,34 @@
         border-radius: 5px;
         text-transform: capitalize;
       }
+    }
+  }
+
+  .btn-group {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 1rem;
+
+    .btn {
+      padding: 1rem 1.5rem 1rem;
+      text-transform: uppercase;
+      color: white;
+      border-radius: 5px;
+      font-size: 16px;
+      width: 95%;
+      text-align: center;
+      i {
+        margin-right: 0.5rem;
+      }
+    }
+    // find a better color
+    .source {
+      background-color: #0077b6;
+      margin-right: 15px;
+    }
+    .build {
+      background-color: #4facfe;
     }
   }
 }
