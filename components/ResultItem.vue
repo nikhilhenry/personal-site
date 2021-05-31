8bb126103db984fb2ce4a885ef2780aae5fc55d1
Nikhil <nikhilanthonyhenry@gmail.com>
2021-05-31 12:43:17 +0530

    create result component

---
+++ components/ResultItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="result">
+    <h2 class="title">{{ result.title }}</h2>
+    <span class="data">{{ createdAt }}</span>
+  </div>
+</template>
+
+<script>
+import formatDate from '../mixins/formatDate'
+export default {
+  name: 'ResultItem',
+  mixins: [formatDate],
+  props: {
+    result: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return { raw_createdAt: '' }
+  },
+  created() {
+    this.raw_createdAt = this.result.createdAt
+  },
+}
+</script>
