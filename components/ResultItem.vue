9eea67bf1895cefe6ced9a3fb697773e67fd1b25
Nikhil <nikhilanthonyhenry@gmail.com>
2021-05-31 12:50:49 +0530

    add styling

--- components/ResultItem.vue
+++ components/ResultItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="result">
     <h2 class="title">{{ result.title }}</h2>
-    <span class="data">{{ createdAt }}</span>
+    <span class="date">{{ createdAt }}</span>
   </div>
 </template>
 
@@ -24,3 +24,22 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.result {
+  padding: 1rem;
+  border: 1px solid #e5e7eb;
+  border-radius: 5px;
+  margin-top: 1rem;
+
+  .title {
+    color: #424242;
+    font-size: 20px;
+    margin-bottom: 0.5rem;
+  }
+
+  .date {
+    color: #616161;
+  }
+}
+</style>
