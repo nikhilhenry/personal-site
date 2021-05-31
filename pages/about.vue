94c1a46a3cfee0013e247719973dd817c674af11
Nikhil <nikhilanthonyhenry@gmail.com>
2021-05-31 13:42:55 +0530

    create about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,42 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <nuxt-content :document="page" class="content" />
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content, error }) {
+    const page = await $content('/about')
+      .fetch()
+      .catch(() => {
+        return error({ statusCode: 404, message: 'Article not found' })
+      })
+    return { page }
+  },
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+}
+
+.container {
+  padding: 2rem 0 1rem !important;
+}
+
+::v-deep .nuxt-content {
+  h1 {
+    margin: 1rem 0 1rem;
+    font-size: 3rem;
+  }
+
+  p {
+    line-height: 24px;
+    font-size: 20px;
+    margin: 24px 0;
+  }
 }
 </style>
