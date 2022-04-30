4582f57a269004e098237781bca3ac22abd1beb1
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-04-30 16:40:41 +0530

    exclude path to function,enable function as server file

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,9 @@ export default {
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: 'static',
+  generate:{
+    exclude:['/api','/api/hello']
+  },
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
